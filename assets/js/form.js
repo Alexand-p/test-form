5d26617795feebd87cecbf0e071fe5a82118ffd3
Alexand-p <3082303@gmail.com>
2022-11-17 14:39:19 +0300

    delet form.js

--- assets/js/form.js
+++
@@ -1,8 +0,0 @@
-var formElement = document.querySelector('.formValidation'); // извлекаем элемент формы
-formElement.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(formElement); // создаём объект FormData, передаём в него элемент формы
-  // теперь можно извлечь данные
-  const number = formData.get('number');
-  console.log(number);
-});
